fd00e14bffa9c4868a7a150e773b4ab7f3c3e9a7
Alessandro Gerelli <geroale2000@gmail.com>
2024-10-08 15:13:37 +0200

    fix:ios fix

--- ios/AppIcons.mm
+++ ios/AppIcons.mm
@@ -7,34 +7,45 @@ RCT_EXPORT_METHOD(setAppIcon:(NSString *)iconName
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
-  if(![[UIApplication sharedApplication] supportsAlternateIcons]) {
-    reject(@"UNSUPPORTED", nil, nil);
-  }
-  
-  [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
-    if (error != nil) {
-      reject(@"ERROR", error.localizedDescription, nil);
-      return;
-    }
-    
-    resolve(nil);
-  }];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if(![[UIApplication sharedApplication] supportsAlternateIcons]) {
+            reject(@"UNSUPPORTED", nil, nil);
+        }
+        
+        NSString* currentIcon = [[UIApplication sharedApplication] alternateIconName];
+        
+        if ([currentIcon isEqualToString:iconName]) {
+            resolve(nil);
+            return;
+        }
+        
+        [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+            if (error != nil) {
+                reject(@"ERROR", error.localizedDescription, nil);
+                return;
+            }
+            
+            resolve(nil);
+        }];
+    });
 }
 
 RCT_EXPORT_METHOD(resetAppIcon:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
-  if(![[UIApplication sharedApplication] supportsAlternateIcons]) {
-    reject(@"UNSUPPORTED", nil, nil);
-  }
-  
-  [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
-    if (error != nil) {
-      reject(@"ERROR", error.localizedDescription, nil);
-      return;
-    }
-    
-    resolve(nil);
-  }];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if(![[UIApplication sharedApplication] supportsAlternateIcons]) {
+            reject(@"UNSUPPORTED", nil, nil);
+        }
+        
+        [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
+            if (error != nil) {
+                reject(@"ERROR", error.localizedDescription, nil);
+                return;
+            }
+            
+            resolve(nil);
+        }];
+    });
 }
 @end
