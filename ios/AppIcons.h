14467ea5b6cd61d43b2cac4c3b0390ce81865ca6
Alessandro Gerelli <geroale2000@gmail.com>
2024-12-19 12:00:22 +0100

    Fix

--- ios/AppIcons.h
+++ ios/AppIcons.h
@@ -1,12 +1,5 @@
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNAppIconsSpec.h"
-
-@interface AppIcons : NSObject <NativeAppIconsSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface AppIcons : NSObject <RCTBridgeModule>
-#endif
 
 @end
